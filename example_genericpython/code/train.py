842c71bd057eaf8ffa3ea34d84d14f429f06934d
Dylan F <dylan.t.fossl@gmail.com>
2022-02-24 17:33:52 -0800

    some examples added

---
+++ example_genericpython/code/train.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+from sklearn import linear_model
+from sklearn import datasets
+import pickle as pl
+import numpy as np
+
+# """
+# makes a container for generic entry
+# docker create --name python_interactive -v C:\Users\dfoss\Documents\Projects\LabDockerExample\example_genericpython\code:/code -it generic_python
+
+# makes a generic training container
+# docker create --name train_model -v C:\Users\dfoss\Documents\Projects\LabDockerExample\example_genericpython\code:/code generic_python python train.py
+# """
+
+print("Hello")
+
+diabetes = datasets.load_diabetes()
+
+# Pick just one feature 
+X = diabetes.data[:, np.newaxis, 2]
+
+# creating and saving some model
+regr = linear_model.LinearRegression()
+regr.fit(X, diabetes.target)
+pl.dump(regr, open('modle2.pl', 'wb'))
+
---
+++ example_vscode/code/test.py
@@ -0,0 +1,19 @@
+#%%
+print("hello world")
+
+# %%
+def add(a,b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a + b
+# %%
+add(1,2)
+# %%
+#Coolio
